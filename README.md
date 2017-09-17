52aad80fb20bc052d80802eb39084ef13fd5fa76
Christian Brandel <mail@cbrandel.de>
2017-09-17 14:09:53 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # php-tester
+A collection of a few php pages that can be useful in exploring/debugging
+a server environment.
+## Installation
+Just put the files in public/ somewhere in your webroot.
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<html>
+<head></head>
+<body>
+[<a href="index.php">Home</a>]
+<h1>PHP Tester</h1>
+php Version: <?php echo phpversion();?> <a href="phpinfo.php">Detailled info</a><br/>
+<a href="tester.php">Request info</a><br/>
+</body>
+</html>
